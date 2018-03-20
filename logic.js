f2726f3d02a217d5d350af8865609246825d2697
Tia Sirrine <tiasirrine@Tias-MacBook-Pro.local>
2018-03-19 17:27:25 -0700

    moment.js added

--- logic.js
+++ logic.js
@@ -47,22 +47,45 @@ var config = {
   });
 
   //create firebase event for adding employee to the database and a row in the html current train scheudle
-  database.ref().on("child_added", function(childSnapshot, prevChildKey){
+  
+  //database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
-      console.log(childSnapshot.val());
+      //console.log(childSnapshot.val());
     
-    var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination;
-    var trainTime = childSnapshot.val().time;
-    var frequency = childSnapshot.val().frequency;
+    //var trainName = childSnapshot.val().name;
+    //var destination = childSnapshot.val().destination;
+    //var trainTime = childSnapshot.val().time;
+    //var frequency = childSnapshot.val().frequency;
 
-    console.log(trainName);
-    console.log(destination);
-    console.log(trainTime);
-    console.log(frequency);
+    //console.log(trainName);
+    //console.log(destination);
+    //console.log(trainTime);
+    //console.log(frequency);
 
+    //child database firebase
+  database.ref().orderByChild("dateAdded").limitToLast(100).on("child_added", function(snapshot) {
+  //pull the values from firebase
+  var trainName = snapshot.val().name;
+  var destination = snapshot.val().destination;
+  var trainTime = snapshot.val().time;
+  var frequency = snapshot.val().frequency;
+  //convert time
+  var firstTimeConverted = moment(trainTime, "HH:mm a").subtract(1, "years");
+  console.log(firstTimeConverted);
+  var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log(timeDiff);
+  //divide minutes / frequency = get remainder
+  var remainder = timeDiff % frequency;
+  console.log(remainder);
+  //subtract current time from the first train value in minutes
+  var minsAway = frequency - remainder;
+  console.log(minsAway);
+  //add time till next train to current = time of next train
+  var timeOfNext = moment().add(minsAway, "minutes");
+  //format for military time
+  var nextTrainMil = moment(timeOfNext).format("HH:mm");
 
     //adds data to train table
-    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + trainTime + "</td></tr>");
+    $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrainMil+ "</td><td>" + minsAway +"</td></tr>");
 
   });
